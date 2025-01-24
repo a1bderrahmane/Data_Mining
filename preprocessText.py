46a44fa15bd24918184233449da05078666515d7
a1bderrahmane <abderrahmaneetudier@gmail.com>
2025-01-24 08:33:10 +0100

    adding adjectives removal

--- preprocessText.py
+++ preprocessText.py
@@ -1,3 +1,33 @@
+# import pandas as pd
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.stem import SnowballStemmer
+# import spacy
+# import re
+
+# nltk.download('stopwords')
+# stop_words = set(stopwords.words('french'))
+# stemmer = SnowballStemmer('french')
+# nlp = spacy.load('fr_core_news_md')
+# def process_text(text):
+#     if text is None or pd.isna(text):
+#         return ''
+#     if not isinstance(text, str):
+#         text = str(text)
+#     tokens = [word for word in text.split() if word.lower() not in stop_words]
+#     stemmed_tokens = [stemmer.stem(word) for word in tokens]
+#     doc = nlp(" ".join(stemmed_tokens))
+#     lemmatized_tokens = [token.lemma_ for token in doc]
+#     return " ".join(lemmatized_tokens)
+
+
+
+
+# def clean_text(text):
+#     if text is None or pd.isna(text):
+#         return ''
+#     cleaned_text = re.sub(r'[^a-zA-ZÀ-ÿ\s]', ' ', text)  # Keeps only letters and spaces
+#     return cleaned_text
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
@@ -9,6 +39,7 @@ nltk.download('stopwords')
 stop_words = set(stopwords.words('french'))
 stemmer = SnowballStemmer('french')
 nlp = spacy.load('fr_core_news_md')
+
 def process_text(text):
     if text is None or pd.isna(text):
         return ''
@@ -20,11 +51,15 @@ def process_text(text):
     lemmatized_tokens = [token.lemma_ for token in doc]
     return " ".join(lemmatized_tokens)
 
-
-
-
 def clean_text(text):
     if text is None or pd.isna(text):
         return ''
     cleaned_text = re.sub(r'[^a-zA-ZÀ-ÿ\s]', ' ', text)  # Keeps only letters and spaces
     return cleaned_text
+
+def remove_adjectives(text):
+    if text is None or pd.isna(text):
+        return ''
+    doc = nlp(text)
+    filtered_tokens = [token.text for token in doc if token.pos_ != 'ADJ']
+    return " ".join(filtered_tokens)
